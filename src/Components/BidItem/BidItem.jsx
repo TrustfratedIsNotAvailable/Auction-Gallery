25e7a39738037dfe84ccc566e68b790e168802d6
TrustfratedIsNotAvailable <nusratmithila524@gmail.com>
2025-04-09 23:39:39 +0600

    3:load bid items to table dynamically

--- src/Components/BidItem/BidItem.jsx
+++ src/Components/BidItem/BidItem.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { FaRegHeart } from "react-icons/fa6";
 
 const BidItem = ({bidItem}) => {
     
-    console.log(bidItem);
+    console.log(bidItem.image);
     return (
-        <div>
-        </div>
+        <tr>
+            <td className='flex items-center gap-2'>
+                 <img src={bidItem.image} 
+                    alt={bidItem.title} 
+                    style={{ width: '50px', height: '50px', objectFit: 'cover' }}
+                />
+                {bidItem.title}
+            </td>
+            <td>${bidItem.currentBidPrice}</td>
+            <td>{bidItem.timeLeft}</td>
+            <td><FaRegHeart /></td>
+            
+        </tr>
     );
 };
 
--- src/Components/BidItems/BidItems.jsx
+++ src/Components/BidItems/BidItems.jsx
@@ -12,9 +12,26 @@ const BidItems = () => {
     
     return (
         <div>
-           {
-            bidItems.map(bidItem => <BidItem key={bidItem.id} bidItem={bidItem}></BidItem>)
-           } 
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th>Items</th>
+                        <th>Current Bid</th>
+                        <th>Time Left</th>
+                        <th>Bid Now</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            bidItems.map(bidItem => (
+                                <BidItem key={bidItem.id} bidItem={bidItem} />
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
